0b89e078335b22d6ed7f4e7b9692941b07e7ffa0
DmitriiMiasnikov <dmitriy.miasnikov@flatstack.com>
2021-06-08 11:59:47 +0300

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+
+import { firebaseCloudMessaging } from './worker/index';
+
+// const HomePage = () => {
+//   useEffect(() => {
+//     setToken();
+//     // this is working
+//     if ('serviceWorker' in navigator) {
+//       navigator.serviceWorker.addEventListener('message', event => console.log('event for the service worker', event));
+//     }
+//     async function setToken() {
+//       try {
+//         const token = await firebaseCloudMessaging.init();
+//         if (token) {
+//           console.log('token', token);
+//           // not working
+//           getMessage();
+//         }
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     }
+//   });
+//   function getMessage() {
+//     console.log('message functions');
+//     const messaging = firebase.messaging();
+//     messaging.onMessage(message => console.log('foreground', message));
+//   }
+//   return (
+//           <div className="capitalize" data-cy="welcome-page">
+//             Welcome to React Next Base
+//           </div>
+//   );
+// };
+
 function App() {
+
+  useEffect(() => {
+    setToken();
+    // this is working
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.addEventListener('message', event => console.log('event for the service worker', event));
+    }
+    async function setToken() {
+      try {
+        const token = await firebaseCloudMessaging.init();
+        if (token) {
+          console.log('token', token);
+          // not working
+          getMessage();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+  function getMessage() {
+    console.log('message functions');
+    const messaging = firebase.messaging();
+    messaging.onMessage(message => console.log('foreground', message));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. kek
         </p>
---
+++ src/worker/index.js
@@ -0,0 +1,54 @@
+import 'firebase/messaging';
+import firebase from 'firebase/app';
+import localforage from 'localforage';
+// self.__WB_DISABLE_DEV_LOGS = true;
+// const util = require('./util');
+// util();
+const firebaseCloudMessaging = {
+  //checking whether token is available in indexed DB
+  tokenInlocalforage: async () => {
+    return localforage.getItem('fcm_token');
+  },
+  //initializing firebase app
+  init: async function() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        databaseURL: 'https://react-next-base.firebaseio.com',
+        apiKey: 'AIzaSyDzObPF0TEJ4G80DFUnQzRDwy8oNQiTH4k',
+        authDomain: 'react-next-base.firebaseapp.com',
+        projectId: 'react-next-base',
+        storageBucket: 'react-next-base.appspot.com',
+        messagingSenderId: '641828025180',
+        appId: '1:641828025180:web:f35fafa05226e1c29a8799',
+        measurementId: 'G-L8P1Q136V9',
+      });
+      try {
+        const messaging = firebase.messaging();
+        const tokenInLocalForage = await this.tokenInlocalforage();
+        //if FCM token is already there just return the token
+        if (tokenInLocalForage !== null) {
+          return tokenInLocalForage;
+        }
+        //requesting notification permission from browser
+        const status = await Notification.requestPermission();
+        if (status && status === 'granted') {
+          //getting token from FCM
+          const fcm_token = await messaging.getToken({
+            vapidKey: 'BN-wMGpdhUIylZonRIx-GGjwuFCPlp_BK9cxb8Cu8pO47FpBrZbKNkRMJIU9vdbUqh3O2XV80QkrZv8f93_ivu4',
+          });
+          if (fcm_token) {
+            //setting FCM token in indexed db using localforage
+            localforage.setItem('fcm_token', fcm_token);
+            console.log('fcm token', fcm_token);
+            //return the FCM token after saving it
+            return fcm_token;
+          }
+        }
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    }
+  },
+};
+export { firebaseCloudMessaging };
